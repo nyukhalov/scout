dd02a1e15826cf4589376d6f3f1ae1821cf8d9fa
Roman Niukhalov <r.nyukhalov@gmail.com>
2021-02-10 21:49:08 +0800

    Update setup script for Jetson Nano

--- scripts/setup-nano.sh
+++ scripts/setup-nano.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-apt install -y curl git docker python3 python3-pip
+set -e
+
+# install tools
+apt update && apt install -y git python3-pip
+pip3 install setuptools
+
+# configure rootless docker
 groupadd docker
-gpassed -a $USER docker
+usermod -aG docker $USER
 newgrp docker
 
+# install ds4drv
 git clone https://github.com/chrippa/ds4drv.git
 cd ds4drv
 python3 setup.py sdist
@@ -15,6 +22,7 @@ udevadm trigger
 cd ..
 rm -r ds4drv
 
+# make ds4drv a service
 curl -o /etc/systemd/system/ds4drv.service https://raw.githubusercontent.com/nyukhalov/scout/master/systemd/ds4drv.service
 systemctl enable ds4drv.service
 systemctl start ds4drv.service
