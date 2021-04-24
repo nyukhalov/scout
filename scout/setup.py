7ef1ed7f3fa5fbee9ad297aa5ccb2aacbf7089fc
Roman Niukhalov <r.nyukhalov@gmail.com>
2021-04-24 11:17:03 +0800

    Fix inability to import scout subpackages

--- scout/setup.py
+++ scout/setup.py
@@ -2,7 +2,14 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['scout'],
+    # the package list is not recursive and all sub-packages must be listed
+    # here in order to be installed
+    packages=[
+        'scout',
+        'scout.lib',
+        'scout.lib.driver',
+        'scout.ros',
+    ],
     package_dir={'': 'src'}
 )
 
